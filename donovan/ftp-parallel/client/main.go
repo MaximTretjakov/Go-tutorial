52a2a3b36a3a49ef9ca4017d1677b857d963d798
MAXIM <victor-kovalov@yandex.ru>
2022-03-13 20:39:45 +0300

    Create response for client, download file

--- donovan/ftp-parallel/client/main.go
+++ donovan/ftp-parallel/client/main.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 	var input string
+	const BUFFER_SIZE = 1024
+	fromServer := make([]byte, BUFFER_SIZE)
 
 	conn, err := net.Dial("tcp", "localhost:8000")
 	if err != nil {
@@ -16,13 +18,23 @@ func main() {
 	defer conn.Close()
 
 	for {
-		fmt.Print("\n\nAvailable commands:\nls - list\ncd - change directory\nget - get file\nclose - close connection\n\n")
 		fmt.Scanln(&input)
+
 		if input == "close" {
 			break
+		}
+		if input == "help" {
+			fmt.Print("\n\nAvailable commands:\nls - list\ncd - change directory\nget - get file\nclose - close connection\n\n")
+			continue
 		}
 		if len(input) > 0 {
 			_, _ = conn.Write([]byte(input))
 		}
+
+		bytesRead, errRead := conn.Read(fromServer)
+		if errRead != nil {
+			log.Println(err)
+		}
+		fmt.Println(string(fromServer[0:bytesRead]))
 	}
 }
--- donovan/ftp-parallel/main.go
+++ donovan/ftp-parallel/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -9,7 +10,30 @@ import (
 	"strings"
 )
 
-func changeDir(path string) {
+func downloadFile(fileName string, conn net.Conn) {
+	var seek int64 = 0
+	const BUFFER_SIZE = 1024
+	fileBuffer := make([]byte, 1024)
+
+	file, err := os.Open(strings.TrimSpace(fileName))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		_, err1 := file.ReadAt(fileBuffer, seek)
+		seek += BUFFER_SIZE
+		fmt.Println(fileBuffer)
+		conn.Write(fileBuffer)
+		if err1 == io.EOF {
+			break
+		}
+	}
+
+	file.Close()
+}
+
+func changeDir(path string, conn net.Conn) {
 	if len(path) > 0 {
 		if err := os.Chdir(path); err != nil {
 			log.Println(err)
@@ -19,10 +43,12 @@ func changeDir(path string) {
 			log.Println(err)
 		}
 		log.Println("CWD: ", cwd)
+		conn.Write([]byte(cwd))
 	}
 }
 
-func listing() {
+func listing(conn net.Conn) {
+	data := "ls: "
 	files, err := ioutil.ReadDir(".")
 	if err != nil {
 		log.Fatal(err)
@@ -30,12 +56,14 @@ func listing() {
 
 	for _, file := range files {
 		fmt.Println(file.Name())
+		data += file.Name() + "\t"
 	}
+	conn.Write([]byte(data))
 }
 
 func handler(c net.Conn) {
 	buf := make([]byte, 20)
-	var commandAndData []string
+	var command []string
 	defer c.Close()
 
 	for {
@@ -47,18 +75,19 @@ func handler(c net.Conn) {
 
 		data := string(buf[0:bytes_data])
 		if len(data) > 0 && strings.ContainsAny(data, "_") {
-			commandAndData = strings.Split(string(buf[0:bytes_data]), "_")
-			if commandAndData[0] == "cd" {
-				log.Printf("Remote: %s Bytes: %d Command: %s Param: %s\n", c.RemoteAddr().String(), bytes_data, commandAndData[0], commandAndData[1])
-				changeDir(commandAndData[1])
+			command = strings.Split(string(buf[0:bytes_data]), "_")
+			if command[0] == "cd" {
+				log.Printf("Remote: %s Bytes: %d Command: %s Param: %s\n", c.RemoteAddr().String(), bytes_data, command[0], command[1])
+				changeDir(command[1], c)
 			}
-			if commandAndData[0] == "get" {
-				log.Printf("Remote: %s Bytes: %d Command: %s Param: %s\n", c.RemoteAddr().String(), bytes_data, commandAndData[0], commandAndData[1])
+			if command[0] == "get" {
+				log.Printf("Remote: %s Bytes: %d Command: %s Param: %s\n", c.RemoteAddr().String(), bytes_data, command[0], command[1])
+				downloadFile(command[1], c)
 			}
 		} else {
 			if data == "ls" {
 				log.Printf("Remote: %s Bytes: %d Command: %s\n", c.RemoteAddr().String(), bytes_data, "ls")
-				listing()
+				listing(c)
 			}
 			if data == "close" {
 				log.Printf("Remote: %s Bytes: %d Command: %s\n", c.RemoteAddr().String(), bytes_data, "close")
