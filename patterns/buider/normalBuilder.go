ae6a30a138a9c9f1e501ebadc6d41d046617d1e2
Maxim Tretyakov <victor-kovalov@yandex.ru>
2022-05-07 16:46:32 +0300

    Add builder pattern

---
+++ patterns/buider/normalBuilder.go
@@ -0,0 +1,27 @@
+package main
+
+type normalBuilder struct {
+	windowType string
+	doorType   string
+	floor      int
+}
+
+func (n *normalBuilder) setWindowType() {
+	n.windowType = "Wooden Window"
+}
+
+func (n *normalBuilder) setDoorType() {
+	n.doorType = "Wooden Door"
+}
+
+func (n *normalBuilder) setNumFloor() {
+	n.floor = 2
+}
+
+func (n *normalBuilder) getHouse() house {
+	return house{
+		windowType: n.windowType,
+		doorType:   n.doorType,
+		floor:      n.floor,
+	}
+}
