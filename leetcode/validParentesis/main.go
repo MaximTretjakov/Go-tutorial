5d75a855a8acbc5dfb20bc96e25d17a810fdac63
Maxim Tretyakov <victor-kovalov@yandex.ru>
2022-08-17 20:04:15 +0300

    Add remove duplicates

--- leetcode/validParentesis/main.go
+++ leetcode/validParentesis/main.go
@@ -1,115 +1,34 @@
 package main
 
-import (
-	"fmt"
-)
-
-// func isValid(s string) bool {
-// 	match := false
-// 	if len(s)%2 == 0 {
-// 		for i := range s {
-// 			if match {
-// 				match = false
-// 				continue
-// 			}
-// 			if s[i] == '(' {
-// 				if s[i+1] == ')' {
-// 					match = true
-// 				} else {
-// 					return false
-// 				}
-// 			} else if s[i] == '[' {
-// 				if s[i+1] == ']' {
-// 					match = true
-// 				} else {
-// 					return false
-// 				}
-// 			} else if s[i] == '{' {
-// 				if s[i+1] == '}' {
-// 					match = true
-// 				} else {
-// 					return false
-// 				}
-// 			} else {
-// 				return false
-// 			}
-// 		}
-// 	} else {
-// 		return false
-// 	}
-// 	return true
-// }
-
-func lenTwo(arr string, open rune, closed rune) bool {
-	if rune(arr[0]) == open {
-		if rune(arr[1]) == closed {
-			return true
-		}
-	}
-	return false
-}
-
-func searchSecond(arr string, ch rune) bool {
-	// s2 := "{[]}"
-	// s3 := "()[]{}"
-	counter := 0
-	for i := range arr {
-		if ch == rune(arr[i+1]) {
-			counter++
-		}
-	}
-	return false
-}
+import "fmt"
 
 func isValid(s string) bool {
-	if len(s)%2 == 0 {
-		if len(s) == 2 {
-			if lenTwo(s, '(', ')') {
-				return true
-			}
-			if lenTwo(s, '[', ']') {
-				return true
-			}
-			if lenTwo(s, '{', '}') {
-				return true
-			} else {
-				return false
-			}
-		} else {
-			for i := range s {
-				if s[i] == '(' {
-					if searchSecond(s, '(') {
-						return true
-					} else {
-						return false
-					}
-				} else if s[i] == '{' {
-					if searchSecond(s, '{') {
-						return true
-					} else {
-						return false
-					}
-				} else if s[i] == '[' {
-					if searchSecond(s, '[') {
-						return true
-					} else {
-						return false
-					}
-				}
-
-			}
-		}
-	} else {
+	if len(s)%2 != 0 {
 		return false
 	}
-	return true
+
+	st := []rune{}
+	open := map[rune]rune{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+
+	for _, r := range s {
+		if closer, ok := open[r]; ok {
+			st = append(st, closer)
+			continue
+		}
+		l := len(st) - 1
+		if l < 0 || r != st[l] {
+			return false
+		}
+		st = st[:l]
+	}
+	return len(st) == 0
 }
 
 func main() {
-	// s1 := "([)]"
-	s2 := "{[]}"
-	// s3 := "()[]{}"
-	// s4 := "()"
-	// s5 := "("
-	fmt.Println(isValid(s2))
+	s1 := "([])"
+	fmt.Println(isValid(s1))
 }
