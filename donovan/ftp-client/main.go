71c2969e6f71aac09c3ffb49d127c151edd07707
Maxim Tretyakov <victor-kovalov@yandex.ru>
2022-11-10 23:41:55 +0300

    Add get, ls, close commands

--- donovan/ftp-client/main.go
+++ donovan/ftp-client/main.go
@@ -5,24 +5,34 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:9001")
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide host:port.")
+		return
+	}
+
+	CONNECT := arguments[1]
+	c, err := net.Dial("tcp", CONNECT)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer conn.Close()
 
 	for {
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Println(">>>")
+		fmt.Print(">> ")
 		text, _ := reader.ReadString('\n')
-		fmt.Fprintf(conn, text+"\n")
+		fmt.Fprintf(c, text+"\n")
 
-		message, _ := bufio.NewReader(conn).ReadString('\n')
+		message, _ := bufio.NewReader(c).ReadString('\n')
 		fmt.Print("->: " + message)
+		if strings.TrimSpace(string(text)) == "STOP" {
+			fmt.Println("TCP client exiting...")
+			return
+		}
 	}
-
 }
