4c38c33dd511d3957c78245887519e310404daae
Maxim Tretyakov <victor-kovalov@yandex.ru>
2022-12-04 22:32:14 +0300

    Add comment

--- leetcode/mergeSortedArray/main.go
+++ leetcode/mergeSortedArray/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+// Лучший вариант решения O(m)
+// func merge(nums1 []int, m int, nums2 []int, n int)  {   
+//     i, j, k := m-1, n-1, m+n-1
+//     for ; j>=0; k-- {
+//         if i >=0 && nums1[i] > nums2[j] {
+//             nums1[k] = nums1[i]
+//             i--
+//         } else {
+//             nums1[k] = nums2[j]
+//             j--
+//         }
+//     }
+// }
+
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	counter := 0
 	for ind, val := range nums1 {
