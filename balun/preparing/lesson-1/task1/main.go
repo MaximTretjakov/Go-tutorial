40eb63b87d31d117c89af8733e71a086be58d17b
Maxim <victor-kovalov@yandex.ru>
2025-03-13 11:55:54 +0300

    [task1] Add Mul function

--- balun/preparing/lesson-1/task1/main.go
+++ balun/preparing/lesson-1/task1/main.go
@@ -21,11 +21,12 @@ func main() {
 	// var signed int8 = math.MaxInt8 + 1
 	// var unsigned uint8 = math.MaxUint8 + 1
 
-	var maxInt int8 = math.MaxInt8
-	var minInt int8 = math.MinInt8
-	fmt.Println(Inc(maxInt))
-	fmt.Println(Dec(minInt))
-	fmt.Println(Add(1, 1))
+	// var maxInt int8 = math.MaxInt8
+	// var minInt int8 = math.MinInt8
+	// fmt.Println(Inc(maxInt))
+	// fmt.Println(Dec(minInt))
+	// fmt.Println(Add(1, 1))
+	fmt.Println(Mul(26, 127))
 }
 
 func Inc(counter int8) (int8, error) {
@@ -54,4 +55,26 @@ func Add(lhs, rhs int8) (int8, error) {
 	}
 
 	return lhs + rhs, nil
+}
+
+func Mul(lhs, rhs int8) (int8, error) {
+	if lhs == 0 || rhs == 0 {
+		return 0, nil
+	}
+
+	if lhs == 1 || rhs == 1 {
+		return lhs * rhs, nil
+	}
+
+	if (lhs == -1 && rhs == math.MinInt8) || (rhs == -1 && lhs == math.MinInt8) {
+		return 0, ErrIntOverflow
+	}
+
+	if lhs > math.MaxInt8/rhs {
+		return 0, ErrIntOverflow
+	} else if lhs < math.MinInt8/rhs {
+		return 0, ErrIntOverflow
+	}
+
+	return lhs * rhs, nil
 }
